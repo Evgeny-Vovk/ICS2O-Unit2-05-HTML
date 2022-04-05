a53364b3881d7467c0dcd0134c554eeb1817ce5a
Evgeny-Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-04-05 16:36:39 +0000

    code

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@
 
 'use strict'
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates the payment you should get.
  */
 function calculate () {
   // input
-  const length = parseInt(document.getElementById('length-of-rectangle').value)
-  const width = parseInt(document.getElementById('width-of-rectangle').value)
+  const hours = parseInt(document.getElementById('hours-worked').value)
+  const rate = parseInt(document.getElementById('hourly-rate').value)
+  const taxes = 0.18
 
   // process
-  const area = length * width
-  const perimeter = 2 * (length + width)
+  const payment = (hours * rate) * (1 - taxes)
+  const government = (hours + rate) * taxes
 
   // output
-  document.getElementById('area').innerHTML = '<p>Area is: ' + area + ' cm².</p>'
-  document.getElementById('perimeter').innerHTML = '<p>Perimeter is: ' + perimeter + ' cm.</p>'
+  document.getElementById('payment').innerHTML = '<p>Your pay will be: $' + payment.toFixed(2) + '</p>'
+  document.getElementById('government').innerHTML = '<p>The government will take: $' + government.toFixed(2) + '</p>'
 }
